c361bae31819a04521a57a136a2c785cf0d85a3e
Renat0z <133365914+Renat0z@users.noreply.github.com>
2024-07-29 10:55:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import streamlit as st
 from modules.contratos import main as contratos_main
 from modules.upload_excel import main as excel_main
+from modules.listar_planilhas import main as listar_planilhas_main
 
 def main():
     st.sidebar.title("Menu")
-    option = st.sidebar.selectbox("Selecione a funcionalidade", ["Gerenciamento de Contratos PDF", "Upload de Planilhas Excel"])
+    option = st.sidebar.selectbox("Selecione a funcionalidade", ["Gerenciamento de Contratos PDF", "Upload de Planilhas Excel", "Listar Planilhas Excel"])
 
     if option == "Gerenciamento de Contratos PDF":
         contratos_main()
     elif option == "Upload de Planilhas Excel":
         excel_main()
+    elif option == "Listar Planilhas Excel":
+        listar_planilhas_main()
 
 if __name__ == "__main__":
     main()
